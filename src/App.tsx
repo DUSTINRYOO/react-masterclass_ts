9f6f9de1713a9c81cc988da2eb50bf0d05b65e6c
DUSTINRYOO <hyeony77@gmail.com>
2023-01-14 13:30:05 -0500

    Set Typescript
    install typed styled components
    and change the rootconst root = ReactDOM.createRoot(
      document.getElementById("root") as HTMLElement
    );

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+const Father = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
+  align-items: center;
+`;
+const Anima = keyframes`
+0% {
+ transform: rotate(0deg);
+ border-radius: 0px;
+}
+50% {
+  transform: rotate(360deg);
+  border-radius: 50px;
+}
+100% {
+  transform: rotate(0deg);
+  border-radius: 0px;
+}
+`;
+const Box = styled.div`
+  display: flex;
+  background-color: ${(props) => props.theme.boxColor};
+  height: 100px;
+  width: 100px;
+  margin: 30px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  animation: ${Anima} 3s linear infinite;
+`;
+
+const Emoji = styled.p`
+  font-size: 30px;
+  margin: 0;
+`;
+
+const Text = styled.h3`
+  color: ${(props) => props.theme.textColor};
+  span {
+    font-size: 50px;
+    margin: 0;
+    &:hover {
+      font-size: 100px;
+      transition: 2s;
+    }
+  }
+  ${Emoji} {
+    font-size: 60px;
+  }
+  span {
+    font-size: 50px;
+    &:hover {
+      font-size: 100px;
+      transition: 2s;
+    }
+  }
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: ${(props) => props.theme.boxColor};
+  width: 100px;
+  margin: 20px;
+`;
+
+const Btn = styled.button`
+  color: ${(props) => props.theme.textColor};
+  background-color: ${(props) => props.theme.boxColor};
+  border: none;
+  border-radius: 15px;
+  width: 100px;
+  margin: 20px;
+  font-size: 20px;
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box>
+        <Text>
+          <span>😍</span>
+          <Emoji>😎</Emoji>
+        </Text>
+      </Box>
+      <Circle>
+        <Text>BRIZONS</Text>
+      </Circle>
+      <Btn>Let's</Btn>
+      <Btn as="a" href="/">
+        Go
+      </Btn>
+      <Emoji>😎</Emoji>
+      <Input />
+    </Father>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+const darkTheme = {
+  textColor: "#144e5a",
+  backgroundColor: "#1d1b23",
+  boxColor: "#ffb901",
+};
+
+const lightTheme = {
+  textColor: "#ffb901",
+  backgroundColor: "#ebe3cc",
+  boxColor: "#144e5a",
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
